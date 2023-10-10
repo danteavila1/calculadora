6ae397a6e422ec05c596379f74e0406a34e50db1
Dante Avila <danteavila1@hotmail.com>
2023-10-10 00:35:14 -0300

    made the function that replace the operator for the last one chosed if theres already one on screen

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-let n1 = 10;
-let n2 = 2;
-const operador = "/";
+let ultimoOperador = null;
+let n1;
+let n2;
+let ultimoEnPantalla;
+
+const botonNumeros = document.querySelectorAll('[data-number]');
+const botonOperador = document.querySelectorAll('[data-operator]');
+const display = document.getElementById('display');
+const igual = document.getElementById('igual');
+
+botonOperador.forEach((boton) => 
+    boton.addEventListener('click', () => anexarOperador(boton.textContent))
+)
+
+botonNumeros.forEach((boton) => 
+    boton.addEventListener('click', () => anexarNumero(boton.textContent))
+)
+
+igual.addEventListener('click', () =>
+    evaluar();
+    operar(n1, n2, operador);
+)
+
+
+function anexarNumero(numero){
+    if (display.textContent == 0){
+        limpiarDisplay();
+    }
+    display.textContent += numero;
+    ultimoEnPantalla = display.textContent;
+}
+
+function limpiarDisplay(){
+    display.textContent = '';
+}
+
+function anexarOperador(operador){
+    evaluar();
+    console.log(n1,' ', n2)
+    if(ultimoOperador != null){
+        display.textContent = ultimoDisplay;
+        display.textContent += operador;
+        ultimoEnPantalla = '';
+    } else {
+        ultimoDisplay = display.textContent;
+        display.textContent += operador;
+        ultimoOperador = operador;
+        ultimoEnPantalla = '';
+    }
+}
+
+function evaluar (){
+    if (n1 == null) {
+        n1 = ultimoEnPantalla;
+    } else {
+        n2 = ultimoEnPantalla;
+    }
+}
 
 function suma (n1, n2){
     res = n1 + n2;
@@ -38,6 +93,7 @@ function operar (operador, n1, n2){
             break;
     }
 }
+
 
 
 
